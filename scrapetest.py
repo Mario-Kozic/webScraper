05cc9b42fbca5e44dd7f58e98c0683651b5cb6f8
[Mario Kozic] <[mkozic89@gmail.com]>
2026-04-20 15:53:21 -0400

    importing HTTPError and URLError from urllib

--- scrapetest.py
+++ scrapetest.py
@@ -1,8 +1,18 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
+try:
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
+    html = urlopen("http://pythonscraping.com/pages/page1.html")
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print('The server could not be found!')
+else:
+    print('It Worked!')
+
 bs = BeautifulSoup(html.read(), 'lxml')
 # print(html.read())
 print(bs.h1, bs.body)
