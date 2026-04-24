929dfa483d1ea45bc03cb9b974893946cce26e87
[Mario Kozic] <[mkozic89@gmail.com]>
2026-04-23 20:44:45 -0400

    testibh find function

--- scraperOne.py
+++ scraperOne.py
@@ -1,9 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html.read(), 'html.parser')
 
-nameList = bs.find_all('span', {'class': 'green'})
-for name in nameList:
-    print(name.get_text())
+# nameList = bs.find_all('span', {'class': 'green'})
+# for name in nameList:
+#     print(name.get_text())
+
+for child in bs.find('table', {'id': 'giftList'}).children:
+    print(child)
+
+for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
+    print(sibling, 'sibling')
+
+print(bs.find('img', {'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
